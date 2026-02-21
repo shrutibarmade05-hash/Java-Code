be18466a6bfddcc3cb494d3d6b453eecee13f9bb
Shruti Barmade <shrutibarmade05@gmail.com>
2026-02-22 00:36:54 +0530

    Add files via upload

---
+++ javaoperators.java
@@ -0,0 +1,20 @@
+package com.sb;
+
+public class javaoperators {
+
+	public static void main(String args[]) {
+		int num =15;
+		int num1 = 45;
+		
+		System.out.println(num+num1);
+
+		System.out.println(num-num1);
+
+		System.out.println(num*num1);
+
+		System.out.println(num/num1);
+
+		System.out.println(num%num1);
+
+	}
+}
---
+++ practice.java
@@ -0,0 +1,25 @@
+package com.sb;
+
+public class practice {
+
+	public static void main(String args[]) {
+		System.out.println("Welcome to MGB......!");
+		
+		double amount = 50000;
+		double deposit = 20000;
+		
+		double totalAmount = amount+deposit;
+		
+		System.out.println("TotalAmount---->"+totalAmount);
+		
+		double witdraw = 30000;
+		
+		double finalAmount = amount+deposit+witdraw;
+		
+		
+		System.out.println("FinalAmount---->"+finalAmount);
+		
+		
+		}
+
+}
