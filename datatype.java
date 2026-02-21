1e5c8240f49bc26b592614b3e0068561fe1e7916
Shruti Barmade <shrutibarmade05@gmail.com>
2026-02-21 23:42:03 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,29 @@
+package com.sb;
+
+public class datatype {
+
+public static void main(String args[]) {
+	byte age = 10;
+	short  seatNumber = 23346;
+	int pinCode = 23456784;
+	long adharNo = 232345678908634l;
+
+	float height = 5.1f;
+	double marks = 99.01;
+			
+
+	boolean checkPassed = true;
+    char specialSymbol = '@';
+   
+    System.out.println("student age is ="+age);
+    System.out.println("Student Seat No is =  " + seatNumber);
+    System.out.println("Student Pincode is =  " + pinCode);
+	System.out.println("Student adharNo is =  " + adharNo);
+	System.out.println("Student height is =  " + height);
+	System.out.println("Student marks is =  " + marks);
+	System.out.println("Student Pass/Fail is =  " + checkPassed);
+	System.out.println("Student specialSymbol is =  " + specialSymbol);
+
+
+}
+}
---
+++ datatypesize.java
@@ -0,0 +1,40 @@
+package com.sb;
+
+public class datatypesize {
+
+	public static void main (String args[]){
+
+	System.out.println("Byte size is " + Byte.BYTES + " Byte ");	System.out.println("Short size is " + Short.BYTES + " Byte ");
+	System.out.println("Integer size is " + Integer.BYTES + " Byte ");
+	System.out.println("Long size is " + Long.BYTES + " Byte ");
+	System.out.println("Float size is " + Float.BYTES + " Byte ");
+	System.out.println("Double size is " + Double.BYTES + " Byte ");
+    System.out.println("Character size is " + Character.BYTES + " Byte ");
+    System.out.println("Boolean is not Found ... !");
+
+    System.out.println(Byte.SIZE);
+    System.out.println(Short.SIZE);
+	System.out.println(Integer.SIZE);
+	System.out.println(Long.SIZE);
+	System.out.println(Float.SIZE);
+	System.out.println(Double.SIZE);
+	System.out.println(Character.SIZE);
+	System.out.println("Boolean size is not found ");
+
+	
+	System.out.println(Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+	System.out.println(Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+	System.out.println(Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+	System.out.println(Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+	System.out.println(Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+	System.out.println(Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+	System.out.println((int) Character.MIN_VALUE + " to " + (int) Character.MAX_VALUE);
+	System.out.println("Boolean Not Found");
+
+
+	
+	
+	}
+	
+
+}
