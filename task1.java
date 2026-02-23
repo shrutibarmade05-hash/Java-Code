a5e73e564be77edf143eeed8f7d658a16cf28fff
Shruti Barmade <shrutibarmade05@gmail.com>
2026-02-23 23:36:07 +0530

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,114 @@
+package com.sc;
+
+public class task1 {
+
+public static void main(String args[]) {
+	
+	//------------student class------------
+	
+	String name="Shruti Barmade";
+	int rollNo = 06;
+	int age=21;
+	double Percentage = 85;
+	String city = "Pune";
+	String  course = "Enggineering";
+	long mobileNo = 87679056876l;
+	
+
+	System.out.println("Student name is="+name);
+	System.out.println("Student rollNo is="+rollNo);
+    System.out.println("Student age is="+age);
+	System.out.println("Student Percentage is="+ Percentage);
+	System.out.println("Student city is="+city);
+	System.out.println("Student course is="+course);
+	System.out.println("Student mobile no is="+mobileNo);
+	 System.out.println();
+	
+	
+	//---------employee class--------
+	 
+	String empName="Shruti Barmade";
+	int empId = 101;
+	int empAge = 25;
+	double empSalary = 50000;
+	String empcity = "Mumbai";
+	String companyName = "Google";
+	String Department = "IT";
+	
+	
+    System.out.println(empName);
+	System.out.println(empId);
+    System.out.println( empAge);
+	System.out.println(empSalary);
+	System.out.println(empcity);
+	System.out.println(companyName);
+	System.out.println(Department );
+	 System.out.println();
+	 
+	 
+	 //---------Hospital Class-----------
+	 
+	 String hospitalName = "City Hospital";
+     String hospitalCity = "Mumbai";
+     int totalDoctors = 50;
+     int totalNurses = 120;
+     int totalBeds = 200;
+     String chiefDoctor = "Dr. Mehta";
+     String hospitalType = "Private";
+
+     System.out.println("Hospital name is = " + hospitalName);
+     System.out.println("Hospital city is = " + hospitalCity);
+     System.out.println("Total doctors = " + totalDoctors);
+     System.out.println("Total nurses = " + totalNurses);
+     System.out.println("Total beds = " + totalBeds);
+     System.out.println("Chief doctor = " + chiefDoctor);
+     System.out.println("Hospital type = " + hospitalType);
+
+     System.out.println();
+	
+	
+	//------------Product class----------
+	
+	String productName = "Laptop";
+    int productId = 201;
+    double price = 55000;
+    int quantity = 5;
+    String brand = "Dell";
+    String category = "Electronics";
+    String warranty = "1 Year";
+
+    System.out.println("Product name is = " + productName);
+    System.out.println("Product id is = " + productId);
+    System.out.println("Product price is = " + price);
+    System.out.println("Product quantity is = " + quantity);
+    System.out.println("Product brand is = " + brand);
+    System.out.println("Product category is = " + category);
+    System.out.println("Product warranty is = " + warranty);
+
+    System.out.println();
+    
+    
+    
+    //-----------Book Class------------
+    
+    String bookTitle = "Java Programming";
+    int bookId = 301;
+    String authorName = "James Gosling";
+    double bookPrice = 499;
+    String publisherName = "XYZ Publications";
+    int totalPages = 350;
+    String language = "English";
+
+    System.out.println("Book title is = " + bookTitle);
+    System.out.println("Book id is = " + bookId);
+    System.out.println("Author name is = " + authorName);
+    System.out.println("Book price is = " + bookPrice);
+    System.out.println("Publisher name is = " + publisherName);
+    System.out.println("Total pages are = " + totalPages);
+    System.out.println("Language is = " + language);
+
+    System.out.println();
+    
+}
+
+}
