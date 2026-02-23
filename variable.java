217d6539d67f64f3d1b8efa5ddee6ce70f81f83d
Shruti Barmade <shrutibarmade05@gmail.com>
2026-02-23 22:44:25 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,26 @@
+package com.sc;
+
+public class variable {
+
+	
+		public static void main(String args[]) {
+			String name="Shruti Barmade";
+			Long adharCardNo=364578901234l;
+			int age = 21;
+			String prnNo="67787887J";
+			
+			System.out.println("Student name is="+name);
+			
+			System.out.println("Student adharCardNo is="+adharCardNo);
+
+			System.out.println("Student age is="+age);
+			
+			System.out.println("Student  prnNo is="+ prnNo);
+
+			
+		}
+
+	
+
+
+}
