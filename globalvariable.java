0e6ec91764d750a37e521176f65306e33788b362
Shruti Barmade <shrutibarmade05@gmail.com>
2026-02-24 23:36:57 +0530

    Add files via upload

---
+++ globalvariable.java
@@ -0,0 +1,108 @@
+package com.sc;
+
+public class globalvariable {
+
+/*	//---------hospital class----------
+	
+	 static String hospitalName = "City Hospital";
+	 static String hospitalCity = "Pune";
+	 static int totalDoctors = 50;
+	 static int totalNurses = 120;
+	 static int totalBeds = 200;
+	 static String chiefDoctor = "Dr. shruti";
+	 static  String hospitalType = "Private";
+	 
+	 
+	/* public static void main(String args[]) {
+		 
+		 System.out.println("Hospital name is = " + hospitalName);
+	     System.out.println("Hospital city is = " + hospitalCity);
+	     System.out.println("Total doctors = " + totalDoctors);
+	     System.out.println("Total nurses = " + totalNurses);
+	     System.out.println("Total beds = " + totalBeds);
+	     System.out.println("Chief doctor = " + chiefDoctor);
+	     System.out.println("Hospital type = " + hospitalType);
+
+	 }
+	 
+	 //-----------Student class-----------
+	 
+	 static  String name="Shruti Barmade";
+	 static 	int rollNo = 06;
+		 static 	int age=21;
+		 static	double Percentage = 95;
+		 static String city = "Pune";
+		 static	String  course = "Enggineering";
+		 static long mobileNo = 87679056876l;
+		
+		 public static void main(String args[]) {
+			 
+			 System.out.println("Student name is="+name);
+				System.out.println("Student rollNo is="+rollNo);
+			    System.out.println("Student age is="+age);
+				System.out.println("Student Percentage is="+ Percentage);
+				System.out.println("Student city is="+city);
+				System.out.println("Student course is="+course);
+				System.out.println("Student mobile no is="+mobileNo);
+				 System.out.println();
+		 }
+	 
+	 
+	 //----------Bank Class-----------
+	 
+	 static String bankName = "MGB";
+	 static String branchName = "Kasar Shirshi";
+	 static String ifscCode = "MAHG0004322";
+	 static String country = "INDIA";
+	 
+	 public static void main(String args[]) {
+		 
+		 System.out.println("Bank name is="+bankName);
+		 System.out.println("Bank branch name is="+branchName);
+		 System.out.println("Bank ifsc code is="+ifscCode);
+		 System.out.println("Bank country is="+country);
+		 		 
+	 
+	 
+	 }
+	 
+	 
+	//---------employee class--------
+	 
+	
+	    static String companyName = "TCS";
+	    static String companyLocation = "Pune";
+	    static String department = "IT";
+	    static int workingHours = 8;
+	    static String companyType = "Private";
+	    static String country = "India";
+	    static double bonusPercentage = 10.5;
+
+	    public static void main(String args[]) {
+
+	        System.out.println("Company Name: " + companyName);
+	        System.out.println("Location: " + companyLocation);
+	        System.out.println("Department: " + department);
+	        System.out.println("Bonus %: " + bonusPercentage);
+	    }
+		 */
+	
+	
+	//-------------library class-------------
+	
+
+	    
+	    static String libraryName = "City Library";
+	    static String city = "Pune";
+	    static int totalBooks = 5000;
+	    static int totalStaff = 10;
+	    static String openingTime = "9 AM";
+	    static String closingTime = "6 PM";
+
+	     public static void main(String args[]) {
+	
+	        System.out.println("Library Name: " + libraryName);
+	        System.out.println("City: " + city);
+	        System.out.println("Total Books: " + totalBooks);
+	    }
+}
