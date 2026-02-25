cd9ee0876cb68916d462e4398cac321dfbafa46f
Shruti Barmade <shrutibarmade05@gmail.com>
2026-02-26 00:01:49 +0530

    Add files via upload

---
+++ Operatortask2.java
@@ -0,0 +1,21 @@
+package com.op;
+
+public class Operatortask2 {
+
+	public static void main(String args[]) {
+		int a= 40;
+		int b=20;
+		int c = a+b;
+		int d = c%2;
+		
+		System.out.println(a+20);  //60
+		System.out.println(a+b);   //60
+		System.out.println(c/a);   //1
+		System.out.println(d+3);   //3
+		System.out.println(d/c);   //0
+		
+		
+	}
+
+	
+}
