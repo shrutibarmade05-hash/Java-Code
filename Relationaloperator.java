88de365bf52aaffe9dce5a55fdabbabe60ea974e
Shruti Barmade <shrutibarmade05@gmail.com>
2026-02-27 17:25:11 +0530

    Add files via upload

---
+++ Relationaloperator.java
@@ -0,0 +1,20 @@
+package com.op;
+
+public class Relationaloperator {
+
+	public static void main(String args[]) {
+	int a = 21;
+	int b = 41;
+	boolean v = a>b;
+	
+	System.out.println(a>b);
+	 System.out.println(a<b);
+	 System.out.println(a>=b);
+	 System.out.println(a<=b);
+	 System.out.println(a==b);
+	 System.out.println(a!=b);
+	 System.out.println(v);
+	 
+	 
+	}
+}
