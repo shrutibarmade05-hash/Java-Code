eadd43d710a1d54f7ad29724ea7f177482625d58
Shruti Barmade <shrutibarmade05@gmail.com>
2026-02-28 10:20:34 +0530

    Add files via upload

---
+++ Incrementdecrement.java
@@ -0,0 +1,87 @@
+package com.op;
+
+public class Incrementdecrement {
+
+	public static void main(String args[]) {
+	//--------1st type-----------
+		
+	/*	int a = 67;
+		System.out.println(a++);
+		System.out.println(++a);	
+		
+		System.out.println(a--);
+		System.out.println(--a);
+		
+		
+		
+		
+		int b = 59;
+		b--;         		//58
+		b++;                //59
+
+		System.out.println(b--);   //59   //58
+		--b;                  //57
+		System.out.println(b++);   //57  //58
+		--b;                   //57
+
+
+		
+		
+		int w = 7;
+		System.out.println(w++); // 7 // 8
+		--w; // 7
+		w++; // 8
+		System.out.println(w++); // 8 // 9
+		w--; // 8
+		System.out.println("_-----------------");
+
+	
+	
+	
+		int  x = w + 5;
+		System.out.println(w + 5); // 13
+
+		w--; //  7
+
+		--w; // 6
+		System.out.println(w++); //  6 // 7
+
+		System.out.println("-------------------------");
+		System.out.println(w);
+		*/
+		
+		
+		
+		//----------------------task----------------------------------------------
+		
+		int i =90;
+		i++;
+		int j =i+9;
+		System.out.println(i++);    //91
+		--j; 
+		System.out.println(j*i);    //9108
+		i++;
+		--i;
+		
+		System.out.println(--i);     //91
+		++i;
+		System.out.println(j+3);     //102
+
+		j--;
+		++j;
+		System.out.println(j+7);      //106
+		j--;
+		i++;
+		--i;
+
+		System.out.println(i+j);      //190
+		System.out.println(i);        //92
+
+		
+		
+		   
+		
+			
+		
+	}
+}
