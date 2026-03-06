5c72aaf410f18999ec8cb1112754476f6f20ab4f
Shruti Barmade <shrutibarmade05@gmail.com>
2026-03-06 12:39:04 +0530

    Add files via upload

---
+++ ifelseladder.java
@@ -0,0 +1,74 @@
+package com.src;
+
+public class ifelseladder {
+	public static void main (String args[]) {
+		
+		//-----------1 . Check Even or Odd Number-----------
+		
+/*		int num= 35;
+		
+		if(num%2==0) {
+			System.out.println("Odd No - " + num);
+			
+       }else {
+	  System.out.println("Even No - " + num);
+       }
+
+		
+		
+		
+	//  --------2 . Check if a Person Can Vote------------------
+		
+		int age = 45;
+		if(age>=18) {
+			System.out.println("Eligible For Voting   ");
+		}else {
+			System.out.println(" Not Eligible For Voting   ");
+		}
+		
+		
+		
+  //---------- 3 . Positive or Negative Number------------
+		
+		int num  = 5;
+		
+		if(num >0) {
+			System.out.println("number is positive  ");
+		}
+		else {
+			System.out.println("number is negative  ");
+		}
+
+		
+		
+	//	-----------4 . Write a program to check whether a number is positive, negative, or zero.-----------
+	/// 
+		int num  =-58;
+		
+		if(num >0) {
+			System.out.println("number is positive  ");
+		}
+		else if(num<0) {
+			System.out.println("number is negative  ");
+			
+		}else {
+			System.out.println("number is 0  ");
+		}
+			*/
+		
+		
+		
+	//	5 . Write a program to compare two numbers and print the greater one.-------------------
+		
+		int num1 = 70;
+		int num2 = 78;
+		
+		if(num1>num2) {
+			System.out.println("num1 is greater than num2  ");
+		} else if(num1<num2){
+			System.out.println("num1 is less than num2  ");
+		}else {
+			System.out.println("num1 is equals to num2  ");
+		}
+    }
+}
