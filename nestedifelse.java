00a2e730706e8169e918e2cc8d4a535b671f2ebf
Shruti Barmade <shrutibarmade05@gmail.com>
2026-03-14 23:54:58 +0530

    Add files via upload

---
+++ nestedifelse.java
@@ -0,0 +1,168 @@
+package com.src;
+
+public class nestedifelse {
+
+	public static void main(String[] args) {
+		
+		//------------if else ladder---------------------------------------
+		
+		
+
+//		if (false || true && true) {
+//			System.out.println("RRR");
+//		} else if (true || true) {
+//			System.out.println("Tere Naam");
+//		} else {
+//			System.out.println("Ready ");
+//		}
+//	
+	
+	
+	
+//	if (10 > 8 || 9 != 1 && 10 == 10) {
+//		System.out.println("RRR");
+//	} else if (true || 1 >= 0) {
+//		System.out.println("Tere Naam");
+//	} else {
+//		System.out.println("Ready ");
+//	}
+		
+		
+		
+		
+//		int marks = 888;
+		
+//				if (marks >= 90 && marks <= 100) {
+//					System.out.println("Grade A");
+//				} else if (marks >= 80 && marks < 90) {
+//					System.out.println("Grade B");
+//				} else if (marks >= 70 && marks < 80) {
+//					System.out.println("Grade C");
+//				} else if (marks >= 60 && marks < 70) {
+//					System.out.println("Grade D");
+//				} else if (marks >= 50 && marks < 60) {
+//					System.out.println("Grade E");
+//				} else {
+//					System.out.println("Grade F -> Fail ... !");
+//				}
+		
+		
+		
+		
+		//------------nested if -----------------------
+		
+		
+//		if (true) {
+//			System.out.println("RRR");
+//			if (false) {
+//				System.out.println("Tere Naam");
+//			} else {
+//				System.out.println("Holiday");
+//			}
+//		} else {
+//			System.out.println("Time Story ");
+//		}
+		
+		
+		
+		
+//		if (true) {
+//			System.out.println("RRR");
+//			if (true && true) {
+//				System.out.println("Tere Naam");
+//				if (true || false) {
+//					System.out.println("PK");
+//				}
+//			} else if (true) {
+//				System.out.println("Holiday");
+//			} else {
+//				System.out.println("WWW");
+//			}
+//		} else {
+//			System.out.println("Time Story ");
+//		}
+//	}
+
+
+		
+		
+		
+//		if (10 > 56 || 10 < 8) {
+//			System.out.println("RRR");
+//			if (10 == 10 && true) {
+//				System.out.println("Tere Naam");
+//				if (true || 45 != 0) {
+//					System.out.println("PK");
+//				}
+//			} else if (3 == 3) {
+//				System.out.println("Holiday");
+//			} else {
+//				System.out.println("WWW");
+//			}
+//		} else {
+//			System.out.println("Time Story ");
+//		}
+//	}
+		
+		
+		
+		
+//		int age = 56;
+//		double salary = 49000;
+//
+//		if (age >= 18) {
+//			System.out.println("1st Stage Clear move to Next Step");
+//			if (salary >= 45000) {
+//				System.out.println("Loan Approved ...!");
+//			} else {
+//				System.out.println("Sorry >>> ! ");
+//			}
+//		} else {
+//			System.out.println("UR NOT ELIGIBLE ... !");
+//		}}
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}	
+	
+}
