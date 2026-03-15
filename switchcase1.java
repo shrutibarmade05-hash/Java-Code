88acda1dd3108d18a5decd5af24e8bc75b566615
Shruti Barmade <shrutibarmade05@gmail.com>
2026-03-15 16:29:28 +0530

    Add files via upload

---
+++ switchcase1.java
@@ -0,0 +1,313 @@
+package com.src;
+
+public class switchcase1 {
+
+	public static void main (String args[]) {
+		
+		//---------------Input color name (Red, Yellow, or Green) and print the appropriate action:------------
+		
+	/*	  String color = "Red";
+
+	switch(color) {
+	case "Red":
+		System.out.println("Stop......!");
+		break;
+	case "Green":
+		System.out.println(" Go.....!");
+		break;
+	case "Yellow":
+		System.out.println("Ready...!");
+		break;
+		
+		default:
+			System.out.println("Invalid Data............!");
+	}
+		
+		
+		
+
+//---------------1. Write a Java program that takes an integer (1–7) and prints the corresponding day name (e.g., 1 → Monday)----------
+		
+		int day = 2;
+		
+		switch(day) {
+		case 1 :
+			System.out.println("Monday");
+			break;
+			
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		
+		case 3 :
+			System.out.println("Wednesday");
+			break;
+			
+		case 4 :
+			System.out.println("Thuresday");
+			break;
+			
+		case 5 :
+			System.out.println("Friday");
+			break;
+			
+		case 6 :
+			System.out.println("Saturday");
+			break;
+			
+		case 7 :
+			System.out.println("Sunday");
+			break;
+			
+			default:
+				System.out.println("Invalid data");
+	}
+		
+		
+//-------------------2. Accept a number (1–12) from the user and print the name of the month using a switch case.-----------		
+		
+		int month = 6;
+		
+		switch(month) {
+		case 1 :
+			System.out.println("January");
+			break;
+			
+		case 2:
+			System.out.println("February");
+			break;
+		
+		case 3 :
+			System.out.println("March");
+			break;
+			
+		case 4 :
+			System.out.println("April");
+			break;
+			
+		case 5 :
+			System.out.println("May");
+			break;
+			
+		case 6 :
+			System.out.println("June");
+			break;
+			
+		case 7 :
+			System.out.println("July");
+			break;
+			
+		case 8 :
+			System.out.println("Auguest");
+			break;
+			
+		case 9 :
+			System.out.println("Septmber");
+			break;
+			
+		case 10 :
+			System.out.println("Octember");
+			break;
+			
+		case 11 :
+			System.out.println("November");
+			break;
+			
+		case 12 :
+			System.out.println("December");
+			break;
+			
+	     default:
+				System.out.println("Invalid data");
+	}
+		
+		
+		
+		
+		
+		//-------------3. Accept two numbers and an operator (+, -, *, /) from the user and perform the operation using a switch case.-------
+		
+		int a = 46;
+		int b = 43;
+		char operator = '*';
+		
+		switch(operator) {
+		case '+':
+			System.out.println(a+b);
+			break;
+			
+		case '-':
+			System.out.println(a-b);
+			break;
+			
+		case '*':
+			System.out.println(a*b);
+			break;
+		
+		case '/':
+			System.out.println(a/b);
+			break;
+			
+			
+	     default:
+				System.out.println("Invalid data");
+		}
+		
+		
+		
+	//-----------------4. Take a grade character (A, B, C, D) as input and print a message:
+             //A → Excellent
+            //B → Good
+           //C → Average
+          //D → Poor	
+		
+		
+		char grade = 'A';
+		
+		switch(grade){
+	case 'A' :
+		System.out.println(" Excellent");
+		break;
+		
+	case 'B' :
+		System.out.println("Good");
+		break;
+		
+	case 'C':
+		System.out.println("Average");
+		break;
+		
+	case 'D':
+		System.out.println("Poor");
+		break;
+	
+     default:
+			System.out.println("Invalid data");
+}
+		
+	
+		
+	//---------------6. Take a single character input and check whether it is a vowel or consonant using a switch statement.------	
+		
+	char ch = 'a';
+	switch(ch) {
+	case 'a':
+	case 'e':
+	case 'i':
+	case 'o':
+	case 'u':
+	case 'A':
+	case 'E':
+	case 'I':
+	case 'O':
+	case 'U':
+		System.out.println("Vowel");
+	break;
+    default:
+			System.out.println("Consonant");
+}
+
+
+		
+	//-----------------7. Use a switch statement to check if a number is even or odd (hint: use number % 2 as the expression)-------
+	int num = 58;
+	switch(num%2) {
+	case 0:
+		System.out.println("Even");
+		break;
+		
+	case 1:
+		System.out.println("Odd");
+		break;
+	}
+		
+		
+//-----------------8. Accept a category type (Domestic, Commercial, Industrial) and print the electricity rate per unit using a switch case.----		
+	
+		 char cat = 'D';  // D = Domestic, C = Commercial, I = Industrial
+
+		    switch(cat) {
+		        case 'D':
+		        	System.out.println("Domestic: 5 Rs/unit\n"); break;
+		        case 'C':
+		        	System.out.println("Commercial: 8 Rs/unit\n"); break;
+		        case 'I': 
+		        	System.out.println("Industrial: 10 Rs/unit\n"); break;
+		        default:
+		        	System.out.println("Invalid category\n");
+		    }
+	
+///-----------------9. Take month number (1–12) and print the corresponding season:
+
+//Dec–Feb → Winter
+//Mar–May → Summer
+//Jun–Aug → Monsoon
+//Sep–Nov → Autumn
+
+	int month= 11;
+	switch(month) {
+	case 12:
+	case 1:	
+	case 2:
+		System.out.println("Winter"); break;
+		
+	case 3:	
+	case 4:	
+	case 5:	
+		System.out.println("Summer"); break;
+		
+	case 6:	
+	case 7:	
+	case 8:	
+		System.out.println("Mansoon"); break;
+		
+	case 9:	
+	case 10:	
+	case 11:	
+		System.out.println("Autumn"); break;
+		
+	default:
+		System.out.println("Invalid data");
+	}
+*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+		
+}
