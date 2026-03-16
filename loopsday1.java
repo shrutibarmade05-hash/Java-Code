d051bae789638901f4847bd8ba4c904c2ae48b67
Shruti Barmade <shrutibarmade05@gmail.com>
2026-03-16 17:12:03 +0530

    Add files via upload

---
+++ loopsday1.java
@@ -0,0 +1,324 @@
+package com.src;
+
+public class loopsday1 {
+
+			public static void main (String args[]) {
+/*	
+	//-----------	Print numbers from 1 to 10 using a loop.---------------------------------------------------
+				//--------for loop---------
+				
+				for(int i=1; i<=10; i++) {
+					System.out.println(i);
+				}
+				
+	
+				//--------while loop------------
+				int i =1;
+				while(i<=10) {
+					System.out.println(i);
+					i++;
+				}
+				
+				
+				//----------do while loop-----------
+				int j =1;
+				do {
+				    System.out.println(j);
+				    j++;
+				} while(j<=10);
+
+
+
+//----------------Print numbers from 10 to 1.------------------------------------------------------------------
+		
+				//--------for loop---------
+			
+				for(int i=10; i>=1; i--) {
+					System.out.println(i);
+				}
+				
+	
+				//--------while loop------------
+				int i =10;
+				while(i>=1) {
+					System.out.println(i);
+					i--;
+				}
+				
+				
+				//----------do while loop-----------
+				int j =10;
+				do {
+				    System.out.println(j);
+				    j--;
+				} while(j>=1);
+
+
+//-------------------------Print all even numbers between 1 and 50.-------------------------------------------------
+				
+				//----------------for loop----------------
+			
+			for(int i =1; i<=50; i++) {
+				if(i%2==0) {
+				System.out.println(i);
+			}}
+			
+			
+			//--------while loop------------
+			int i =1;
+			while(i<=50) {
+				if(i%2==0) {
+				System.out.println(i);
+				}
+				i++;
+				}
+		
+			
+			
+			//----------do while loop-----------
+			int j =1;
+			do {
+				if(j%2==0) {
+			    System.out.println(j);
+				}
+			    j++;
+				} while(j<=50);
+
+	*/		
+	//------------------Print all odd numbers between 1 and 50.--------------------------------------------		
+				/*		
+				
+				for(int i =1; i<=50; i++) {
+					if(i%2!=0) {
+					System.out.println(i);
+			}}
+		
+				//--------while loop------------
+				int i =1;
+				while(i<=50) {
+					if(i%2!=0) {
+					System.out.println(i);
+					}
+					i++;
+					}
+				
+				
+			
+				//----------do while loop-----------
+				int j =1;
+				do {
+					if(j%2!=0) {
+				    System.out.println(j);
+					}
+				    j++;
+					} while(j<=50);
+
+			
+	//---------------------------Print the table of 5 using a loop.---------------------------------------
+
+				//----------------for loop----------------
+				
+		for(int i =1; i<=10; i++) {
+					
+					System.out.println(5*i));
+				}
+				
+				
+				//--------while loop------------
+				int i =1;
+				while(i<=10) {
+				
+					System.out.println(5*i);
+					
+					i++;
+					}
+			
+				
+				
+				//----------do while loop-----------
+				int j =1;
+				do {
+					
+				    System.out.println(5*j);
+					
+				    j++;
+					} while(j<=10);
+			
+	
+	//------------------------------------Print the table of any number (e.g., n = 7).--------------------
+		
+			
+
+				//----------------for loop----------------
+				
+	 n = 7;
+			
+		   for(int i =1; i<=10; i++) {
+					
+					System.out.println(n*i);
+				}
+				
+				
+			//--------while loop------------
+				int i =1;
+				int n = 7;
+				while(i<=10) {
+				
+					System.out.println(n*i);
+					
+					i++;
+					}
+			
+				
+				
+				//----------do while loop-----------
+				int j =1;
+				int n = 7;
+				do {
+					
+				    System.out.println(n*j);
+					
+				    j++;
+					} while(j<=10);
+			
+				
+			
+//-----------------Print Hello 10 times.------------------------			
+			
+			
+
+				for(int i=1; i<=10; i++) {
+					System.out.println("hello");
+				}
+				
+	
+				//--------while loop------------
+				int i =1;
+				while(i<=10) {
+					System.out.println("hello");
+					i++;
+				}
+				
+				
+				//----------do while loop-----------
+				int j =1;
+				do {
+				    System.out.println("hello");
+				    j++;
+				} while(j<=10);
+
+		*/		
+				
+		//------------------------Print the sum of even numbers from 1 to 100.--------------------	
+			
+		//---------	for loop---------------
+				
+	/*		int sum = 0;
+			for(int i = 2; i<=100; i++) {
+				if(i%2==0) {
+					sum = sum+i;
+					System.out.println(sum);
+				}
+			}
+				
+		//--------while loop------------
+			int i = 1;
+			int sum = 0;
+			while(i<=100) {
+				if(i%2==0) {
+					sum=sum+i;
+				}
+				i++;
+			}
+			System.out.println(sum);
+			}
+}	
+	
+		
+	//---------------do while loop------------
+				int i = 1, sum =0;
+				do {
+					if(i%2==0) {
+						sum = sum+i;
+						
+					}i++;
+					
+				}while(i<=100);
+				System.out.println(sum);
+			}
+}
+			
+				
+*/			
+			
+				//------------------------Print the sum of odd numbers from 1 to 100.--------------------			
+			
+				int sum = 0;
+				for(int i = 2; i<=100; i++) {
+					if(i%2!=0) {
+						sum = sum+i;
+						System.out.println(sum);
+					}
+				}
+					
+		/*	//--------while loop------------
+				int i = 1;
+				int sum = 0;
+				while(i<=100) {
+					if(i%2!=0) {
+						sum=sum+i;
+					}
+					i++;
+				}
+				System.out.println(sum);
+				}
+	}	
+		
+			
+		//---------------do while loop------------
+					int i = 1, sum =0;
+					do {
+						if(i%2!=0) {
+							sum = sum+i;
+							
+						}i++;
+						
+					}while(i<=100);
+					System.out.println(sum);
+				}
+	}int sum = 0;
+			for(int i = 2; i<=100; i++) {
+				if(i%2==0) {
+					sum = sum+i;
+					System.out.println(sum);
+				}
+			}
+				
+		//--------while loop------------
+			int i = 1;
+			int sum = 0;
+			while(i<=100) {
+				if(i%2==0) {
+					sum=sum+i;
+				}
+				i++;
+			}
+			System.out.println(sum);
+			}
+}	
+	
+		
+	//---------------do while loop------------
+				int i = 1, sum =0;
+				do {
+					if(i%2==0) {
+						sum = sum+i;
+						
+					}i++;
+					
+				}while(i<=100);
+				System.out.println(sum);
+			}
+}*/
+}
+			}
