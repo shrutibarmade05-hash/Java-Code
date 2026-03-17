81d963bf52dc58b5d8d94b11e8fd1300265d6a63
Shruti Barmade <shrutibarmade05@gmail.com>
2026-03-17 15:08:01 +0530

    Add files via upload

---
+++ loopday2.java
@@ -0,0 +1,134 @@
+package com.src;
+
+public class loopday2 {
+	public static void main(String[] args) {
+//
+	//-----------------even odd------------------------------------
+		
+	/*	//(for loop)
+	
+		for(int i=1;i<=50;i++) {
+			if(i%2==0) {
+				System.out.println("even="+i);	
+			}else {
+		System.out.println("odd="+i);
+			}
+			}
+	
+	
+	
+	//(while loop)
+		
+		int i = 1;
+		while(i<=50) {
+			if(i%2==0) {
+				System.out.print("even="+i);
+			}else {
+				System.out.print("odd="+i);
+			
+			}
+			i++;
+		}
+
+	
+	
+	//do while loop
+		
+		int i = 1;
+		do{
+			if(i%2==0) {
+				System.out.println("even="+i);	
+			}
+				i++;
+			}while(i<=50);
+			
+	
+	
+	
+		
+	
+	
+	
+		for(int i=1; i<=10; i++) {
+			System.out.println(i*i);
+		}
+	
+	
+	
+		for(int i=1; i<=10; i++) {
+			System.out.println(i*i*i);
+		}
+	
+	
+	
+	
+	
+//-----------sum --------------	
+	
+	int sum = 0;
+		for(int i=1; i<=10; i++) {
+			sum = sum+i;
+		System.out.println(sum);
+		}
+	
+		*/	
+		
+		
+//----------factorial no-------------------
+	/*   1             1
+	     1*2           2
+	     1*2*3          6
+         1* 2*3*4       24
+	*/ 
+	
+	
+	int fact =1;
+	for(int i=1; i<=10; i++) {
+		fact = fact*i;
+	System.out.println(fact);
+	}
+	
+	
+	
+	}
+			
+			
+			
+}
+			
+			
+			
+			
+			
+			
+			
+		
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+	
+		
+		
+	
+
