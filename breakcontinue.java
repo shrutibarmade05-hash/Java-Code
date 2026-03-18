c807f4e033e315498fad36a14ce40dfe0355e57e
Shruti Barmade <shrutibarmade05@gmail.com>
2026-03-18 12:26:23 +0530

    Add files via upload

---
+++ breakcontinue.java
@@ -0,0 +1,81 @@
+package com.src;
+
+public class breakcontinue {
+
+	public static void main(String[] args) {
+		
+	//	for (int i = 1; i <= 10; i++) {
+//			if (i % 2 != 0) {
+//				continue; 
+//
+//			}
+//			System.out.println(i);
+//
+//		}
+
+		
+		
+			
+	//		for(int i = 1; i<=18; i++) {
+	//			if(i%2==0) {
+	//				continue; 
+	//			}System.out.println(i);
+	//		}
+		
+		
+		
+		
+//			for (int i = 1; i <= 100; i++) {
+//				if (i == 8) {
+//					break;  
+//			}
+//				System.out.println(i);
+//		}
+	
+		
+		
+//		for (int i = 1; i <= 100; i++) {
+//		if (i%4== 0) {
+//			continue;
+//		}
+//			System.out.println(i);
+//	}
+		
+		
+		
+		
+		
+		
+
+//for (int i = 1; i <= 100; i++) {
+//		if (i%4== 0 || i%10==0) {
+//			continue;
+//	}
+//			System.out.println(i);
+//	}
+		
+
+		
+		
+ for(int i = 10; i>=2; i--) {
+	if(i%3 ==0) {
+		continue;
+	}System.out.println(i);
+}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		}
+}
