15ae58e7f4aeb541fee6899a98c50e2001d96d71
Shruti Barmade <shrutibarmade05@gmail.com>
2026-03-20 14:58:52 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,37 @@
+package obj.com;
+
+public class Student {
+String studentName ;
+int studentId ;
+
+public static void main(String args[]) {
+	
+	
+	Student S1 = new Student();
+	System.out.println("Student Name = "+ "Shruti Barmade");
+	System.out.println("Student Id="+1234);
+	System.out.println("-------------------------------------------------------------------------");
+	
+	Student S2 = new Student();
+	System.out.println("Student Name = "+ "Vijay Shivare");
+	System.out.println("Student Id="+1235);
+	System.out.println("-------------------------------------------------------------------------");
+
+	Student S3 = new Student();
+	System.out.println("Student Name = "+ "Ankita Chopade");
+	System.out.println("Student Id="+1232);
+	System.out.println("-------------------------------------------------------------------------");
+
+	Student S4 = new Student();
+	System.out.println("Student Name = "+ "Vishal Jagatp");
+	System.out.println("Student Id="+1231);
+	
+	
+	
+	
+	
+	
+	
+}
+}
+
