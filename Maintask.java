093512fa90057a172323cb6877b382d2c48fe713
Shruti Barmade <shrutibarmade05@gmail.com>
2026-03-24 15:43:40 +0530

    Add files via upload

---
+++ Maintask.java
@@ -0,0 +1,102 @@
+package com.mtd;
+
+public class Maintask {
+
+	
+	
+	
+	public static void main(String args[]) {
+		
+		//-------------------1. Fibonacci series  ------------------
+		
+	   /*	Methodtask t1= new Methodtask();
+		t1.checkFibonacci() ;
+		
+	
+		
+		//2. even odd series ----------------------------	
+		
+	    Methodtask t1 = new Methodtask();
+		t1.checkEvenodd();
+		
+		
+		
+		//3. reverse no ------------
+		
+		 Methodtask t1 = new Methodtask();
+		 t1.checkReverseno();		
+		
+		
+	
+		//-----------------------5. star pattern -------------------
+		
+		 Methodtask t1 = new Methodtask();
+		 t1.star();	
+	
+	
+	
+
+		//-------------------------7. Myinfo(15 V)--------------------------
+		
+		 Methodtask t1 = new Methodtask();
+		    t1.name = "Shruti Shripad Barmade";
+		    t1. id= 112;
+			t1. rollNo = 6;
+			t1. myClass = "Third Year";
+			t1.city = "Pune";
+			t1. section = "A";
+			t1. collageName = "Siddhant Collage Of Engineering ,Pune";
+			t1. moNo = 8767901030l;
+			t1.marks = 87;
+			t1.emailId = "shrutibarmade05@gmail.com";
+			t1. age = 21;
+			t1. dob = 22-05-2005;
+			t1. bloodGroup = "B+";
+			t1.religion = "Hindu";
+			t1.height = 5.1f;
+		
+		
+		t1.myInformation();
+		
+		
+		
+		//-----------------Create method to add two numbers	---------------
+		
+		 Methodtask c1 = new Methodtask();
+		 System.out.println("Addition = "+ c1. myAddition());
+		 
+		
+	
+	//--------------Create method to find square of number----------------
+		
+		 Methodtask c1 = new Methodtask();
+		 c1.findSquare();
+	}
+	
+	
+	*/	
+	
+	
+	//---------------------return largest of two number--------------
+		
+		Methodtask c1 = new Methodtask();
+	    c1.largestNumber();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}	
+	
+	
+	
+	
+}
---
+++ Methodtask.java
@@ -0,0 +1,145 @@
+package com.mtd;
+
+public class Methodtask {
+	//-----------------------------------1. Fibonacci series  ------------------------
+	
+	/*public void checkFibonacci() {
+		int a = 0; 
+		int b =1;
+		int c ;
+	  
+		for(int i =1; i<=10; i++) {
+        	c = a+b;
+			
+			System.out.println(a+" ");
+			a=b;	
+			b=c;
+						
+			
+		}
+	}
+	
+	
+	
+	
+
+//-----------------------------------2. even odd series ----------------------
+	public void checkEvenodd() {
+		
+	int num = 46;
+	if(num %2==0) {
+		System.out.println("Number is even");
+	
+	}else {
+		System.out.println("Number is odd");
+	}
+
+	
+	
+	
+// -------------------	3. reverse no --------------------
+	
+	public void checkReverseno() {
+   int num = 1245;
+   int rev = 0;
+   while(num>0) {
+	   rev = rev*10+num%10;
+	   num = num/10;
+   }System.out.println(rev);
+   }
+	
+	
+//-----------------------5. star pattern -------------------
+	public void star() {
+		for( int i = 1; i<=5; i++) {
+			for(int j = 1; j<=5; j++) {
+				System.out.print(" *");
+			}System.out.println(" ");
+		}
+	}
+   
+	
+	
+	
+	//-----------------------7. Myinfo(15 V)---------------------------
+	String name ;
+	int id;
+	int rollNo;
+	String myClass;
+	String city;
+	String section;
+	String collageName;
+	long moNo;
+	int marks;
+	String emailId;
+	int age;
+	int dob;
+	String bloodGroup;
+	String religion;
+	float height;
+	
+	
+	public void myInformation() {
+		
+		 System.out.println("Name ="+name );
+		  System.out.println("Id ="+id);
+		  System.out.println("RollNo="+rollNo);
+		  System.out.println("Class ="+myClass);
+		  System.out.println("City ="+city);
+		  System.out.println("Section ="+section);
+		  System.out.println("Collage Name ="+collageName);
+		  System.out.println("Mobile No ="+ moNo);
+		  System.out.println("Marks ="+marks);
+		  System.out.println("Email Id ="+emailId);
+		  System.out.println("Age ="+age);
+		  System.out.println("Date Of Birth ="+dob);
+		  System.out.println("Blood Group ="+bloodGroup );
+		  System.out.println("Religion ="+ religion);
+		  System.out.println("Height ="+height);
+		 
+		
+	}
+	
+	
+	
+	
+	//--------------------Create method to add two numbers--------------------
+	
+	public int myAddition() {
+		int a = 10;
+		int b = 45;
+		int c = a+b;
+		
+		return c ;
+      }
+	
+	
+	//--------------Create method to find square of number----------------
+	
+	public void findSquare() {
+ int num = 5;
+ int square = num*num;
+ System.out.println(square);
+}
+
+
+
+//------------------return largest of two numbers-----------------------------------
+
+*/
+	public void largestNumber() {
+	int a = 10;
+	int b = 24;
+	if(a>b) {
+		System.out.println(a);
+	}else {
+		System.out.println(b);
+	}
+
+
+
+
+
+	}
+
+}
