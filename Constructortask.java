251274e905d5b6aabb7a34765ce8f1cc0b3d5a41
Shruti Barmade <shrutibarmade05@gmail.com>
2026-03-26 14:58:08 +0530

    Add files via upload

---
+++ Constructortask.java
@@ -0,0 +1,90 @@
+package com.sh;
+
+public class Constructortask {
+
+//----------create an class with Name Student --> create an constructor and print whole info about Student---
+	/*
+	public Constructortask(String Fname ,String  Lname , int age , long Mono , String Gender , String Email ,String Course ) {
+		System.out.println("Student First Name--->"+Fname);
+		System.out.println("Student Last Name--->"+Lname);
+		System.out.println("Student Age--->"+age);
+		System.out.println("Student Mobile No--->"+Mono);
+		System.out.println("Student Gender--->"+Gender);
+		System.out.println("Student Email Id--->"+Email);
+		System.out.println("Student Course--->"+Course);
+	}
+	 public void city(String City) {
+		System.out.println("Student City------>"+City);
+	}
+	
+	
+	
+	
+//------------create an class with Name myPortFolio --> create an constructor and print whole info about u----------
+
+	public Constructortask (String Name , int Age , String Gender , String City , String State , String Country , String Course , String CollageName , Long MobileNo , String EmailId) {
+		System.out.println("-------------------------My Portfolio----------------------------");
+		System.out.println("Name--->"+Name);
+		System.out.println("Age--->"+Age);
+		System.out.println("Gender--->"+Gender);
+		System.out.println("City--->"+City);
+		System.out.println("State--->"+State);
+		System.out.println("Country--->"+Country);
+		System.out.println("Course--->"+Course);
+		System.out.println("Collage Name--->"+CollageName);
+		System.out.println("Mobile No--->"+MobileNo);
+		System.out.println("Email Id--->"+EmailId);
+		}
+	
+	
+	
+//-----------create an class with Name Car --> create an constructor and print whole info about car and also add one method-----------
+	
+	public Constructortask (String Name , String Brand , int Model , String Color , int SeatingCapacity ,long Price) {
+		System.out.println("-------------------------Car Information----------------------------");
+		System.out.println("Car Name--->"+Name);
+		System.out.println("Car Brand--->"+Brand);
+		System.out.println("Car Model--->"+Model);
+		System.out.println("Car Color--->"+Color);
+		System.out.println("Car Seating Capacity--->"+SeatingCapacity);
+		System.out.println("Car Price--->"+Price);
+	}
+	public void car(int ModelNumber) {
+		System.out.println("Car Model No------>"+ModelNumber);
+	*/
+	
+	
+//----------create an class with Name Employee --> create an constructor and print whole info about employee also add method (salary)------------
+	
+	
+	public Constructortask (int Id , String Name , int Age , String Gender , long MobileNo , String City , String EmailId ) {
+		
+		System.out.println("-------------------------Employee Information----------------------------");
+		System.out.println("Employee Id--->"+Id);
+		System.out.println("Employee Name--->"+Name);
+		System.out.println("Employee Age--->"+Age);
+		System.out.println("Employee Gender--->"+Gender);
+		System.out.println("Employee Mobile No--->"+MobileNo);
+		System.out.println("Employee Email Id--->"+EmailId);
+		System.out.println("Employee City--->"+City);
+		}
+	
+	public void Employee (int Salary) {
+		System.out.println("Employee Salary--->"+Salary);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ Mainconstructortask.java
@@ -0,0 +1,30 @@
+package com.sh;
+
+public class Mainconstructortask {
+
+public static void main(String args[]) {
+	
+/*	/----------create an class with Name Student --> create an constructor and print whole info about Student---
+	Constructortask s = new Constructortask("Riya" , "Bhosale" , 21, 8767901330l , "Female" , "riyabhosale@gmail.com ", "BE");
+		s.city("Pune");
+	
+	
+	
+//------------create an class with Name myPortFolio --> create an constructor and print whole info about u----------
+	Constructortask s = new Constructortask("Shruti", 21, "Female" , "Latur" , "Maharashtra" , "India" , "BE" , "Siddhant Collage Of Engineering , Pune " , 8767901030l ,"shrutibaramde05@gmail.com");
+	
+
+	
+//-----------create an class with Name Car --> create an constructor and print whole info about car and also add one method-----------
+	
+	Constructortask  c = new Constructortask ("Swift" , "Suzuki" , 2022, "White" , 5 , 800000l);
+	c.car(1234);
+	*/	
+	
+	
+//----------create an class with Name Employee --> create an constructor and print whole info about employee also add method (salary)------------
+		
+	Constructortask  c = new Constructortask (123 , "Vijay Shivare" , 25 ,"Male" , 8767901030l ,"Pune" ,"vijayshivare@gmail.com");
+	c.Employee(50000);
+	}
+}
