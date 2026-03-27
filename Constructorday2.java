19e4208ffa17124a77de3daffb1c0a1ce70a078d
Shruti Barmade <shrutibarmade05@gmail.com>
2026-03-27 15:28:30 +0530

    Add files via upload

---
+++ Constructorday2.java
@@ -0,0 +1,173 @@
+package com.sh;
+
+/* ---------------------------------Student Information------------------------------------------------------
+ 
+ 
+public class Constructorday2 {
+    int studentId;
+    String studentName;
+    int age;
+    String city;
+    
+	public Constructorday2( int studentId ,String studentName ,  int age ,  String city) {
+		this.studentId = studentId ;
+		this.studentName = studentName;
+		this.age = age;
+		this.city = city;
+	}
+	
+	public Constructorday2(Constructorday2 s) {
+		this.studentId = studentId ;
+		this.studentName = studentName;
+		this.age = age;
+		this.city = city;
+	}
+	
+	
+	public void viewInffo() {
+		System.out.println(studentId + "   " + studentName + "  " +age +"  "+ city );
+	}
+}
+
+
+
+
+
+//-----------------------------------------Employee Infromation-------------------------------------------------
+
+public class Constructorday2 {
+	int employeeId;
+	String empName ;
+	String city;
+	double salary;
+	long mobileNo;
+	
+	public Constructorday2(int employeeId,String empName ,String city,double salary ,long mobileNo) {
+    		this.employeeId = employeeId;
+    		this.empName = empName;
+    		this.city = city;
+    		this.salary  = salary;
+    		this.mobileNo = mobileNo;
+	}
+	
+	public void display() {
+		System.out.println(employeeId + " "+ empName+ " "+city+ " "+salary +" "+mobileNo);
+	}
+}
+
+
+
+
+
+//-----------------------------------------My Portfolio -------------------------------------------------------
+
+public class Constructorday2 {
+String name ;
+int rollNo;
+int age;
+String year;
+String collageName;
+float marks;
+String city;
+
+public Constructorday2(String name , int rollNo, int age, String year, String collageName, float marks, String city) {
+	this. name = name ;
+	this.rollNo = rollNo;
+	this. age = age;
+	this. year  = year;
+	this. collageName = collageName;
+	this.marks = marks;
+	this. city = city;
+}
+public void display() {
+	System.out.println("--------------My Portfolio------------------");
+	System.out.println(name +" "+rollNo +" "+age+" "+year+" "+collageName+" "+marks+" "+city);
+}
+}
+
+
+
+
+
+//----------------------------------------------Informatation about Car---------------------------------------------
+
+public class Constructorday2 {
+String carName ;
+String carBrand ;
+int carModel;
+String carColor;
+int carSeatingcapacity;
+long carPrice;
+
+public Constructorday2(String carName ,String carBrand ,int carModel,String carColor,int carSeatingcapacity,long carPrice) {
+	this. carName  = carName;
+	this. carBrand = carBrand ;
+	this. carModel = carModel;
+	this. carColor = carColor;
+	this. carSeatingcapacity = carSeatingcapacity;
+	this. carPrice = carPrice;
+
+}
+public void display() {
+	System.out.println("-----------------Information About Car----------------------------");
+	System.out.println(carName +" "+carBrand +" "+carModel+" "+carColor+" "+carSeatingcapacity+" "+carPrice);
+}
+}
+*/
+
+
+
+
+
+
+//---------------------------------------------Information About Collage--------------------------------------------
+
+public class Constructorday2 {
+String collageName ;
+String location;
+int establishedYear;
+String collageType;
+String university;
+String approval;
+int intakeCapacity;
+double fees;
+
+public Constructorday2(String collageName , String location, int establishedYear, String collageType, String university, String approval, int intakeCapacity,double fees) {
+	this. collageName   = collageName ;
+	this. location = location ;
+	this. establishedYear = establishedYear;
+	this. collageType = collageType;
+	this. university = university;
+	this. approval = approval;
+	this. intakeCapacity = intakeCapacity;
+	this. fees = fees;
+
+}
+public void display() {
+	System.out.println("-----------------Information About Collage----------------------------");
+	System.out.println(collageName +"      "+location  +"     "+establishedYear+"     "+collageType+"     "+university+"     "+approval+"     "+intakeCapacity+"     "+fees);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Mainconstructorday2.java
@@ -0,0 +1,37 @@
+package com.sh;
+
+public class Mainconstructorday2 {
+public static void main(String args[]) {
+	
+	
+	//------------------------Information About Student------------------------------------
+/*	Constructorday2 c1 = new Constructorday2(123 , "Shruti ",  21 , "Pune");
+	c1.viewInffo();
+	//c1.studentName = "Suru";
+	c1.viewInffo();
+
+	
+	//----------------------Information About Employee----------------------------------------
+	Constructorday2 c1 = new Constructorday2(101 , "Vijay" ,"Latur" , 90000l , 8767999099l );
+	c1.display();
+	
+	
+	//---------------------My Portfolio---------------------------------------------------
+	Constructorday2  c = new Constructorday2 ("Shruti Barmade" ,06 , 21 , "TE IT Third Year " , "Siddhant Collage Of Engineering Pune", 8.76f , "Latur");
+	c.display();
+	
+	
+
+	//------------Information About Car-------------------------------------------------------
+	Constructorday2  c = new Constructorday2("Swift" , "Suzuki" ,  2022 ,"White",5, 800000l);
+	c.display();   */
+	
+	
+	//--------------Information About Collage---------------------------------------
+	Constructorday2  c = new Constructorday2("Siddhant Collage Of Engineering" ,       "Sudumbare,Pune" ,  2005 ,"Private Engineering Collage","Savitribai Phule Pune University","AICTE Approved",600,58500);
+	c.display();
+	
+	
+	
+}
+}
