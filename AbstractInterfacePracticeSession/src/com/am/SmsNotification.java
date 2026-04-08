18316c73012adb5506583e7a21e44eba5d9dc0d7
Shruti Barmade <shrutibarmade05@gmail.com>
2026-04-08 20:31:12 +0530

    Delete AbstractInterfacePracticeSession directory

--- AbstractInterfacePracticeSession/src/com/am/SmsNotification.java
+++
@@ -1,9 +0,0 @@
-package com.am;
-
-public class SmsNotification implements Notification{
-	@Override
-	public void sendMessage() {
-		System.out.println("Notification through via SMS");
-		
-	}
-}
--- AbstractInterfacePracticeSession/src/com/am/WhatsappNotification.java
+++
@@ -1,9 +0,0 @@
-package com.am;
-
-public class WhatsappNotification implements Notification {
-	@Override
-	public void sendMessage() {
-		System.out.println("Notification through via WhatsApp");
-		
-	}
-}
